033f07d78a2fa92e5a1341f865d2495c275a17e2
Joako <Joako@Joako-PC>
2016-04-14 17:44:27 -0300

    Agregue tests para busqueda, falta el metodo en si

--- src/principal/Mapa.java
+++ src/principal/Mapa.java
@@ -1,11 +1,30 @@
 package principal;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class Mapa {
 	
+	List<POI> pois;
 
+	public Mapa(){
+		pois = new ArrayList<POI>();
+	}
 
+	public List<POI> getPOI() {
+		return pois;
+	}
+	public void setPOI(POI poi) {
+		this.pois.add(poi);
+	}
+	public void agregarPOI(POI unPOI){
+		this.pois.add(unPOI);
+	}
 
 	
-
+	public void Buscar(String texto){
+		
+		
+	}
 }
---
+++ src/principal/testBusqueda.java
@@ -0,0 +1,68 @@
+package principal;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import tipos.Banco;
+import tipos.CGP;
+import tipos.Local;
+import tipos.ParadaColectivo;
+import tipos.Rubro;
+import tipos.Servicio;
+
+
+public class testBusqueda {
+	
+	ParadaColectivo parada1;
+	ParadaColectivo parada2;
+	ParadaColectivo parada3;
+	Rubro muebleria;
+	Local mueblesSA;
+	Local mueblesParaTodos;
+	Servicio asesoramiento;
+	Servicio jubilacion;
+	CGP cgp;
+	Banco banco;
+	Mapa mapa;
+	
+	@Before
+	public void initialize(){
+		parada1 = new ParadaColectivo();
+		parada2 = new ParadaColectivo();
+		parada3 = new ParadaColectivo();
+			parada1.setNombre("primer parada de la linea 114");
+			parada2.setNombre("segunda parada de la linea 114");
+			parada3.setNombre("tercera parada de la linea 114");
+		muebleria = new Rubro();
+			mueblesSA = new Local();
+			mueblesParaTodos = new Local();
+				mueblesSA.setRubro(muebleria);
+				mueblesSA.setNombre("muebles sociedad anonima");
+				mueblesParaTodos.setRubro(muebleria);
+				mueblesParaTodos.setNombre("otra muebleria");
+		asesoramiento = new Servicio("asesoramiento");
+		jubilacion = new Servicio("jubilacion");
+			cgp = new CGP();
+			banco = new Banco();
+				cgp.agregarServicio(asesoramiento);
+				cgp.setNombre("CGP nro 1");
+				banco.agregarServicio(asesoramiento);
+				banco.agregarServicio(jubilacion);
+				banco.setNombre("Nacion");
+		mapa = new Mapa();
+			mapa.agregarPOI(parada1);
+			mapa.agregarPOI(parada2);
+			mapa.agregarPOI(parada3);
+			mapa.agregarPOI(mueblesSA);
+			mapa.agregarPOI(mueblesParaTodos);
+			mapa.agregarPOI(cgp);
+			mapa.agregarPOI(banco);
+			
+	}
+	@Test		
+	public void busquedaParadas114(){
+		mapa.Buscar("114");
+	}
+}
