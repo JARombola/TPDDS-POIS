fa2f1fb47f5a4c5b5b87c88fceee2eb1cf5decd1
JULIETA <JULIETA@192.168.0.128>
2016-06-05 21:03:34 -0300

    cambie lo del email para usar una libreria

--- src/main/java/principal/TerminalConEnvioEmailEnLaBusqueda.java
+++ src/main/java/principal/TerminalConEnvioEmailEnLaBusqueda.java
@@ -1,30 +1,57 @@
 package principal;
 
 import otros.Administrador;
-import otros.Mail;
-import otros.MailSender;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
 
 public class TerminalConEnvioEmailEnLaBusqueda {
 	Terminal terminal;
 	double tiempoEsperaMax;
+	private JavaMailSender mailSender;
+	private SimpleMailMessage simpleMailMessage;
+	
 	public TerminalConEnvioEmailEnLaBusqueda(Terminal terminal, double tiempoEecucionMax) {
 		this.tiempoEsperaMax=tiempoEecucionMax;
 		this.terminal = terminal;
 	}
+	public void setSimpleMailMessage(SimpleMailMessage simpleMailMessage) {
+		this.simpleMailMessage = simpleMailMessage;
+	}
+
+	public void setMailSender(JavaMailSender mailSender) {
+		this.mailSender = mailSender;
+	}
+	
 	public  void buscar(String texto1, String texto2){
 		
 		double tiempoEjecucion=	terminal.buscar(texto1, texto2);
 		
 		if(tiempoEjecucion>tiempoEsperaMax){
-			Administrador adminInterno=new Administrador();
-			Mail mail = new Mail();
-			mail.setFrom(adminInterno.getEmail());
-			//agregar parametro Admin
-			//mail.setTo(admin.getEmail());
-			mail.setMessage("tardo mucho la busqueda");
-			mail.setSubject("Aviso busqueda lenta");
-			MailSender mailSender = null ;
-			mailSender.send(mail);
+			MimeMessage message = mailSender.createMimeMessage();
+			
+				Administrador adminInterno=new Administrador();
+			
+			MimeMessageHelper helper;
+				
+			try {
+			
+				helper = new MimeMessageHelper(message, true);
+
+				helper.setFrom(simpleMailMessage.getFrom());
+				helper.setTo(simpleMailMessage.getTo());
+				helper.setSubject(simpleMailMessage.getSubject());
+				helper.setText(String.format(
+					simpleMailMessage.getText(), adminInterno.getEmail(), "tardo mucho la busqueda")); 
+		//	this.mailSender.send(message);
+					
+			} catch (MessagingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
 
