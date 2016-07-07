d25796b3a2116ffeab42be5457b42492580ecb02
Lu Colombo <lucianasolcolombo@msn.com>
2016-07-06 23:47:46 -0300

    agregué una clase en caso de que no se quiera hacer nada con la falla de un proceso

---
+++ src/main/java/procesos/SinManejoDeFalla.java
@@ -0,0 +1,11 @@
+package procesos;
+
+public class SinManejoDeFalla implements ManejoDeResultadosProcesos {
+
+	@Override
+	public void manejarError(Proceso proceso) {
+		// no hace nada :)
+
+	}
+
+}
