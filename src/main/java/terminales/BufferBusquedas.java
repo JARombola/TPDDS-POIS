09b2c09b0198801c31ca7d66cca356bbbb7b933d
Daniela <Daniela@Daniela>
2016-10-18 01:21:08 -0300

    Cache con Redis. Falta ver los asserts de los tests

--- src/main/java/terminales/BufferBusquedas.java
+++ src/main/java/terminales/BufferBusquedas.java
@@ -2,21 +2,29 @@ package terminales;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.Morphia;
 import org.mongodb.morphia.query.Query;
+import org.redisson.Redisson;
+import org.redisson.api.RBucket;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
 
 import com.mongodb.MongoClient;
 
 import externos.InterfazBuscadores;
 import pois.POI;
 
+
 public class BufferBusquedas {
 	
 	private List<POI> resultados;
 	private List<InterfazBuscadores> buscadoresComponentes;
 	private Datastore store;
+	private Config config;
+	private RedissonClient reddison;
 
 	public BufferBusquedas(){
 		Morphia morphia = new Morphia();
@@ -27,25 +35,30 @@ public class BufferBusquedas {
 		store = morphia.createDatastore(mongo, "CACHE");
 		resultados=new ArrayList<POI>();
 		buscadoresComponentes= new ArrayList<InterfazBuscadores>();
+		config = new Config(); 
+		config.setUseLinuxNativeEpoll(false);
+		config.useSingleServer().setAddress("127.0.0.1:6379"); 
+		reddison = Redisson.create(config);
 	}
 	
 	public List<POI> buscar(String texto1, String texto2){		
 		
-		Query<POI> query = store.createQuery(POI.class); 		//TODO:CONSULTA CACHE
-			query.or(query.criteria("direccion.calle").equal(texto1),
-					 query.criteria("direccion.localidad").equal(texto1)); 
-			
-		List<POI> resultadosEnBuffer = query.asList();
-			
-		if(resultadosEnBuffer.isEmpty()){			//Si ninguno sirvió => consulta a los externos
+		RBucket<List<POI>> bucket = reddison.getBucket("texto1");
+		List<POI> resultadosCache = bucket.get();
+		
+		if(resultadosCache == null){			//Si ninguno sirvió => consulta a los externos
+			List<POI> resultadosEnBuffer = new ArrayList<POI>();
 			buscadoresComponentes.forEach(componente -> componente.buscar(texto1, texto2));
 			getBuscadoresComponentes().forEach(componente -> {
 				resultadosEnBuffer.addAll(componente.getResultado());
 				resultados.addAll(componente.getResultado());			//Guardo los del externo
 				}
 			);
+			bucket.set(resultadosEnBuffer);
+			return resultadosEnBuffer;
+		} else {
+			return resultadosCache;
 		}
-		return resultadosEnBuffer;
 	}
 
 	public void agregarExterno(InterfazBuscadores componente) {
